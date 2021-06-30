82be32bb0338824d3078f48636b88fa4512f74f2
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 01:15:03 +0200

    add onload functionality to display all users

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const body = document.getElementsByTagName("body")[0]
+
 const loadData = async() => {
     const data = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = data.json();
     return users;
 }
+
+window.addEventListener("load", async () => {
+    try{
+    const users = await loadData();
+    const ul = document.createElement("ul");
+    ul.setAttribute("id", "user-container");
+    for(user of users) {
+        const li = document.createElement("li")
+        li.innerHTML = `<span>${user.name}</span><span>${user.email}</span><button class="button" id=${user.id}>See more</button>`;
+        ul.appendChild(li);
+    }
+    body.appendChild(ul);
+    } catch(e) {
+        console.log(e);
+    }
+});
