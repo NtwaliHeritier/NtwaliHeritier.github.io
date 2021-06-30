068979dc6d9d2ce5f93aa4f5596c082d55c4287f
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 01:50:48 +0200

    add functionality to view posts by user

--- index.js
+++ index.js
@@ -21,3 +21,31 @@ window.addEventListener("load", async () => {
         console.log(e);
     }
 });
+
+const postLoaded = async(id) => {
+    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`);
+    const posts = data.json();
+    return posts
+}
+
+const loadPostsOnDom = (posts) => {
+    const anchor = document.createElement('a');
+    anchor.setAttribute("href", "./index.html");
+    anchor.textContent = "Back"
+    body.appendChild(anchor);
+    const ul = document.createElement("ul");
+    for(post of posts) {
+        const li = document.createElement("li");
+        li.innerHTML = `<span>${post.title}</span><span>${post.body}</span>`;
+        ul.appendChild(li);
+    }
+    body.appendChild(ul);
+}
+
+body.addEventListener("click", async (e) => {
+    if(e.target.className == "button") {
+        const posts = await postLoaded(e.target.id);
+        document.querySelector("#user-container").remove();
+        loadPostsOnDom(posts)
+    }
+})
