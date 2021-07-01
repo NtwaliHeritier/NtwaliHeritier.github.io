60a3fe56655145efa0ef41f450d43b2c6a764f64
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-01 03:53:56 +0200

    add style to the post page

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ window.addEventListener("load", async () => {
         childDiv.innerHTML = `<span>${user.name}</span>
                             <span>${user.email}</span>
                             <form>
-                                <input type="hidden" name="id" value=${user.id} ></input>
-                                <input type="hidden" name="name" value=${user.name}></input>
-                            <button class="button">See more</button>
+                                <input type="hidden" value=${user.id} name="id"></input>
+                                <input type="hidden" value=${user.name} name="name"></input>
+                                <button class="button">See more</button>
                             </form>`;
         parentDiv.appendChild(childDiv);
     }
@@ -38,29 +38,31 @@ const postLoaded = async(id) => {
 const loadPostsOnDom = (posts, name) => {
     const anchor = document.createElement('a');
     anchor.setAttribute("href", "./index.html");
+    anchor.setAttribute("class", "anchor");
     anchor.textContent = "Back"
     body.appendChild(anchor);
     const title = document.createElement("h3");
     title.textContent=`Posts by ${name}`;
     body.appendChild(title);
-    const ul = document.createElement("ul");
+    const parentDiv = document.createElement("div");
+    parentDiv.setAttribute("class", "user-container-div");
     for(post of posts) {
-        const li = document.createElement("li");
-        li.innerHTML = `<span>${post.title}</span><span>${post.body}</span>`;
-        ul.appendChild(li);
+        const childDiv = document.createElement("div");
+        childDiv.setAttribute("class", "child-div")
+        childDiv.innerHTML = `<h4>${post.title}</h4>
+                                <p>${post.body}</p>`;
+        parentDiv.appendChild(childDiv);
     }
-    body.appendChild(ul);
+    body.appendChild(parentDiv);
 }
 
 body.addEventListener("click", async (e) => {
     if(e.target.className == "button") {
         e.preventDefault();
-        const nameTag = e.target.previousElementSibling
-        const idTag = nameTag.previousElementSibling
-        console.log(idTag)
-        console.log(nameTag)
+        const nameTag = e.target.previousElementSibling;
+        const idTag = nameTag.previousElementSibling;
         const posts = await postLoaded(idTag.value);
         document.querySelector("#user-container").remove();
-        loadPostsOnDom(posts, nameTag.value)
+        loadPostsOnDom(posts, nameTag.value);
     }
 })
